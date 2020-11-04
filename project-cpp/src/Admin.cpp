57b6c532278b7b4db43f821f33fde5507dafe3f3
iagosevero <52476653+iagosevero@users.noreply.github.com>
2020-11-04 07:30:41 -0300

    Update Admin.cpp
    Complementado a função de troca. Adicionado constantes de pacote de conta para a mesma func.

--- project-cpp/src/Admin.cpp
+++ project-cpp/src/Admin.cpp
@@ -1,6 +1,10 @@
 #include "Admin.h"
-
 #include <stdlib.h>
+
+#define TEMPO_B (long)60*60*60
+#define TEMPO_P (long)10
+#define TEMPO_M 1
+#define TEMPO_X 0
 
 Admin::Admin(Display *d, Timer *t, Keyboard *k, Userdatabase *u)
 {
